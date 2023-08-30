780ab1abe94e50a69a35804aeda45e337b566390
abhinandan-lab <airindustry3@gmail.com>
2023-08-30 20:26:26 +0530

    doing sometnig

---
+++ adminhtml/adminmain.js
@@ -0,0 +1,3 @@
+function updateLyricsTabs() {
+    console.log('updateLyricsTab');
+}
--- php/admin/admin_dashboard.php
+++ php/admin/admin_dashboard.php
@@ -3,26 +3,14 @@
 
 include_once 'config.php';
 include_once 'constants.php';
-// include_once 'functions/FormValidation.php';
-// include_once 'functions/Utils.php';
-
 
 
 checkSessionAndRedirect('admin_login_id', false, '/adminindex');
-// 
-// dd($_SESSION);
-
-
 
 ?>
 
 <?php include_once 'admin_header.php' ?>
-<?php include_once 'admin_sidebar.php' ?>
+<?php // include_once 'admin_sidebar.php' ?>
 
-<main>
-    <div class="container-fluid px-4">
-
-    </div>
-</main>
 
 <?php include_once 'admin_footer.php' ?>
